2930de0f3024ed3ecfcab525ca84ae3b3895a7e2
Alexander Khokhlov <radio_swamp@mail.ru>
2023-01-23 22:44:17 +0300

    Added installation packages, git update to the latest version

--- 01_apt_install.sh
+++ 01_apt_install.sh
@@ -4,8 +4,19 @@ sudo apt install -y vim
 sudo apt install -y tmux
 sudo apt install -y screen
 sudo apt install -y mc
+sudo apt install -y curl
+sudo apt install -y binutils
+sudo apt install -y htop
 sudo apt install -y doublecmd-gtk
+sudo apt install -y python3-pip
+pip install --upgrade pip
 
 # example of progress watching: cp GhostBSD.vdi /home/tecmint/Downloads/ & progress -mp $!
 sudo apt install -y progress
+
+# install and upgrade git to the latest
+sudo apt install git
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt-get update
+sudo apt-get install git -y
 
