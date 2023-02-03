c61c2d5d700fd59056c7ad9c7c1eb39156229c75
Alexander Khokhlov <radio_swamp@mail.ru>
2023-02-03 16:50:36 +0300

    improved structure of install_* files for readability

--- install_docker.sh
+++ install_docker.sh
@@ -4,23 +4,25 @@
 
 # docker
 #sudo apt install linux-image-extra-$(uname -r) linux-image-extra-virtual
-sudo apt install linux-image-extra-virtual
-
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y linux-image-extra-virtual
+sudo apt install -y apt-transport-https
+sudo apt install -y ca-certificates
+sudo apt install -y curl
+sudo apt install -y software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-
 sudo apt update && apt-cache policy docker-ce
-
 sudo apt install -y docker-ce
 
 sudo usermod -aG docker $(whoami)
+
 # check
-sudo systemctl status docker
+sudo systemctl status docker --no-pager
 
 # docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+DOCKER_COMPOSE="https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)"
+sudo curl -L ${DOCKER_COMPOSE} -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 # check
 docker-compose --version
--- install_virt.sh
+++ install_virt.sh
@@ -8,13 +8,11 @@ if [[ $value == 0 ]]; then
     echo "Error: Virtual Machine cannot be installed due to file /proc/cpuinfo"
     echo "       contains no instructions 'vmx' or 'svm' allowed."
 else
-    # if so - install
     sudo apt install qemu qemu-kvm libvirt-daemon libvirt-clients bridge-utils virt-manager
     sudo gpasswd -a $USER libvirt
     # check service status
     sudo systemctl status libvirtd --no-pager
     kvm-ok
-
     # addition for ARM
     sudo apt-get install -y qemu-system-aarch64
 fi;
