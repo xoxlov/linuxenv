f399bc9ad4ed2be6a1c002ee78cb0b74eaeae0bf
Alexander Khokhlov <radio_swamp@mail.ru>
2023-03-21 21:57:51 +0300

    Added soft link from python to python3

--- 01_apt_install.sh
+++ 01_apt_install.sh
@@ -30,3 +30,7 @@ git --version
 # http://ytdl-org.github.io/youtube-dl/download.html
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
+
+# add soft link from python to python3
+sudo ln -s /usr/bin/python3 /usr/bin/python
+
