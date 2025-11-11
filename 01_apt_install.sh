f3841f3800eadc3f3d4bbf8bbfb369101c98ab20
Alexander Khokhlov <radio_swamp@mail.ru>
2025-11-11 05:39:10 +0300

    Updated apt installation script

--- 01_apt_install.sh
+++ 01_apt_install.sh
@@ -2,10 +2,25 @@
 
 sudo apt install -y vim
 sudo apt install -y tmux
+sudo apt install -y openssh-server
+sudo systemctl enable ssh
+sudo apt install -y iptables
+sudo apt install -y ufw
 sudo apt install -y mc
 sudo apt install -y curl
 sudo apt install -y neovim
 sudo apt install -y binutils
+sudo apt install -y build-essential
+sudo apt install -y cmake
+sudo apt install -y net-tools
+# sudo apt install -y gcc-9 g++-9 gcc-10 g++-10 gcc-11 g++-11 g++-12 gcc-12 g++-13 gcc-13 g++-14 gcc-14
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 90 --slave /usr/bin/g++ g++ /usr/bin/g++-14 --slave /usr/bin/gcov gcov /usr/bin/gcov-13
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 80 --slave /usr/bin/g++ g++ /usr/bin/g++-13 --slave /usr/bin/gcov gcov /usr/bin/gcov-13
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 100 --slave /usr/bin/g++ g++ /usr/bin/g++-12 --slave /usr/bin/gcov gcov /usr/bin/gcov-12
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 70 --slave /usr/bin/g++ g++ /usr/bin/g++-11 --slave /usr/bin/gcov gcov /usr/bin/gcov-11
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10 --slave /usr/bin/gcov gcov /usr/bin/gcov-10
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 50 --slave /usr/bin/g++ g++ /usr/bin/g++-9 --slave /usr/bin/gcov gcov /usr/bin/gcov-9
+# sudo update-alternatives -y --config gcc
 sudo apt install -y doublecmd-gtk
 sudo apt install -y python3
 sudo apt install -y python3-pip
@@ -22,6 +37,7 @@ sudo apt install -y tcpdump
 sudo apt install -y zsh
 sudo apt install -y fish
 sudo apt install -y sublime-text
+sudo apt install -y bat
 # example of progress watching: cp GhostBSD.vdi /home/tecmint/Downloads/ & progress -mp $!
 sudo apt install -y progress
 
