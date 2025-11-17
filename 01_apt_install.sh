46b6806572e270ac4cfbba021dcb303323a67151
Alexander Khokhlov <radio_swamp@mail.ru>
2025-11-17 22:28:57 +0300

    Add vars, aliases, tmux setup; updated install packages; updated .bashrc

--- 01_apt_install.sh
+++ 01_apt_install.sh
@@ -13,14 +13,6 @@ sudo apt install -y binutils
 sudo apt install -y build-essential
 sudo apt install -y cmake
 sudo apt install -y net-tools
-# sudo apt install -y gcc-9 g++-9 gcc-10 g++-10 gcc-11 g++-11 g++-12 gcc-12 g++-13 gcc-13 g++-14 gcc-14
-#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 90 --slave /usr/bin/g++ g++ /usr/bin/g++-14 --slave /usr/bin/gcov gcov /usr/bin/gcov-13
-#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 80 --slave /usr/bin/g++ g++ /usr/bin/g++-13 --slave /usr/bin/gcov gcov /usr/bin/gcov-13
-#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 100 --slave /usr/bin/g++ g++ /usr/bin/g++-12 --slave /usr/bin/gcov gcov /usr/bin/gcov-12
-#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 70 --slave /usr/bin/g++ g++ /usr/bin/g++-11 --slave /usr/bin/gcov gcov /usr/bin/gcov-11
-#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10 --slave /usr/bin/gcov gcov /usr/bin/gcov-10
-#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 50 --slave /usr/bin/g++ g++ /usr/bin/g++-9 --slave /usr/bin/gcov gcov /usr/bin/gcov-9
-# sudo update-alternatives -y --config gcc
 sudo apt install -y doublecmd-gtk
 sudo apt install -y python3
 sudo apt install -y python3-pip
@@ -40,6 +32,15 @@ sudo apt install -y sublime-text
 sudo apt install -y bat
 # example of progress watching: cp GhostBSD.vdi /home/tecmint/Downloads/ & progress -mp $!
 sudo apt install -y progress
+
+# sudo apt install -y gcc-9 g++-9 gcc-10 g++-10 gcc-11 g++-11 g++-12 gcc-12 g++-13 gcc-13 g++-14 gcc-14
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 90 --slave /usr/bin/g++ g++ /usr/bin/g++-14 --slave /usr/bin/gcov gcov /usr/bin/gcov-13
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 80 --slave /usr/bin/g++ g++ /usr/bin/g++-13 --slave /usr/bin/gcov gcov /usr/bin/gcov-13
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 100 --slave /usr/bin/g++ g++ /usr/bin/g++-12 --slave /usr/bin/gcov gcov /usr/bin/gcov-12
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 70 --slave /usr/bin/g++ g++ /usr/bin/g++-11 --slave /usr/bin/gcov gcov /usr/bin/gcov-11
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 60 --slave /usr/bin/g++ g++ /usr/bin/g++-10 --slave /usr/bin/gcov gcov /usr/bin/gcov-10
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 50 --slave /usr/bin/g++ g++ /usr/bin/g++-9 --slave /usr/bin/gcov gcov /usr/bin/gcov-9
+# sudo update-alternatives -y --config gcc
 
 pip install --upgrade pip
 
--- 02_bashrc.sh
+++ 02_bashrc.sh
@@ -2,10 +2,27 @@
 
 RCFILE=~/.bashrc
 
-echo " " >> ${RCFILE}
-sed -i "s/^alias l='ls -CF'/# alias l='ls -CF'/" ${RCFILE}
-cat ./alias >> ${RCFILE}
-echo "" >> ${RCFILE}
+# tmux setup
+cp ./res/.tmux.conf ~/
+cp -r ./res/.tmux ~/
 
+
+# setup aliases and variables
+cp ./res/.bash_aliases ~/
+cp ./res/.bash_env_var ~/
+
+echo "
+# Additional env vars definitions.
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+# Additional env vars definitions.
+if [ -f ~/.bash_env_var ]; then
+    . ~/.bash_env_var
+fi
+" >> $RCFILE
+
+
+# setup bash_prompt shell plugin
 cat ./bash_git >> ${RCFILE}
-
