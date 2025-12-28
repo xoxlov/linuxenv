3580911d43932db22110353ed80c812b717e3586
Alexander Khokhlov <radio_swamp@mail.ru>
2025-12-28 23:22:07 +0300

    Updated tmux scripts for home and for work

--- res/.tmux/tmx.sh
+++ res/.tmux/tmx.sh
@@ -3,22 +3,25 @@
 SESSION_NAME="xoxlov"
 
 function searchDownloads() {
-  DOWNLOADS="/home/${USER}"
-  if [[ -x "/home/${USER}/Загрузки" ]]; then
-    DOWNLOADS="/home/${USER}/Загрузки"
-  elif [[ -x "/home/${USER}/Downloads" ]]; then
-    DOWNLOADS="/home/${USER}/Downloads"
+  DOWNLOADS="${HOME}"
+  if [[ -x "${HOME}/Загрузки" ]]; then
+    DOWNLOADS="${HOME}/Загрузки"
+  elif [[ -x "${HOME}/Downloads" ]]; then
+    DOWNLOADS="${HOME}/Downloads"
   fi
   export DOWNLOADS=${DOWNLOADS}
 }
 searchDownloads
 
-tmux new-session -s ${SESSION_NAME}   -n admin -d  'cd /home/${USER}/tmp; bash -i'
-tmux new-window  -t ${SESSION_NAME}:1 -n home      'cd /home/${USER}; bash -i'
-tmux new-window  -t ${SESSION_NAME}:2 -n work      'cd /home/${USER}/work; bash -i'
+mkdir -p ${HOME}/tmp
+mkdir -p ${HOME}/work
+
+tmux new-session -s ${SESSION_NAME}   -n admin -d  'cd ${HOME}/tmp; bash -i'
+tmux new-window  -t ${SESSION_NAME}:1 -n home      'cd ${HOME}; bash -i'
+tmux new-window  -t ${SESSION_NAME}:2 -n work      'cd ${HOME}/work; bash -i'
 tmux new-window  -t ${SESSION_NAME}:3 -n download  'cd ${DOWNLOADS}; bash -i'
-tmux new-window  -t ${SESSION_NAME}:4 -n work-zsh  'cd /home/${USER}/work; zsh -i'
-tmux new-window  -t ${SESSION_NAME}:5 -n work-fish 'cd /home/${USER}/work; fish -i'
+#tmux new-window  -t ${SESSION_NAME}:4 -n work-zsh  'cd ${HOME}/work; zsh -i'
+#tmux new-window  -t ${SESSION_NAME}:5 -n work-fish 'cd ${HOME}/work; fish -i'
 #tmux new-window  -t ${SESSION_NAME}:6 -n process   'bash -c "htop --readonly"'
 
 # https://github.com/samoshkin/tmux-config/blob/master/tmux/tmux.conf
