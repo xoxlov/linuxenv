fd8f425953c16218ac875b1b91b31aa7b0178cea
Alexander Khokhlov <radio_swamp@mail.ru>
2026-01-18 20:45:41 +0300

    Add arch installation packages for basic setup

---
+++ arch_02.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+pacman -S --no-confirmation git
+pacman -S --no-confirmation networkmanager
+sudo systemctl enable NetworkManager.service
+sudo systemctl start NetworkManager.service
+pacman -S --no-confirmation tmux
+pacman -S --no-confirmation mc
+pacman -S --no-confirmation binutils
+pacman -S --no-confirmation base-devel
+pacman -S --no-confirmation
+pacman -S --no-confirmation
+pacman -S --no-confirmation
+pacman -S --no-confirmation
+pacman -S --no-confirmation
+pacman -S --no-confirmation
+pacman -S --no-confirmation
+pacman -S --no-confirmation
+pacman -S --no-confirmation
+pacman -S --no-confirmation
+pacman -S --no-confirmation
+pacman -S --no-confirmation
+pacman -S --no-confirmation
+pacman -S --no-confirmation
+pacman -S --no-confirmation
+pacman -S --no-confirmation
+
